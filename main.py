72bb71ef753538e9541f5815f21f1311ebc1c449
Bruna Rocha <bsant4131@gmail.com>
2025-02-04 22:01:04 -0300

    importações de bibliotecas

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import pandas as pd
 import csv
 from datetime import datetime
 
-# class CSV
+class CSV:
+    CSV_ARQUIVO ="dados_financeiros.csv"
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_ARQUIVO)
+        except FileNotFoundError:
+            
