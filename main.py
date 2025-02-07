a60519a165dd32479cbf6e786870b81723bbd77f
Bruna Rocha <bsant4131@gmail.com>
2025-02-06 21:54:10 -0300

    Criando o arquivo para armazenamento dos dados financeiros.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import csv
+import os
 from datetime import datetime
 
+
 class CSV:
-    CSV_ARQUIVO ="dados_financeiros.csv"
+    CSV_FILE ="dados_financeiros.csv"
 
     @classmethod
     def initialize_csv(cls):
         try:
-            pd.read_csv(cls.CSV_ARQUIVO)
+            pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            
+            df = pd.DataFrame(columns=["date", "amount", "category", "description"])
+            df.to_csv(cls.CSV_FILE, index=False)
+
+CSV.initialize_csv()
+
+
+# 20-07-2024, 125, ENtrada, Salário
+
+
